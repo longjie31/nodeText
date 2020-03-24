7dd41c67a91cb9a600f6c63cc4598c6068287af6
longjie <761166384@qq.com>
2020-03-24 18:03:50 +0800

    1

--- server.get.js
+++ server.get.js
@@ -1,7 +1,14 @@
 const http = require('http');
 const fs = require('fs');
+const queryString = require('querystring');
+const urlLib = require('url');
 http.createServer((req, res) => {
+    // true解析url里的query
     console.log(req.url);
+    const obj = urlLib.parse(req.url, true);
+    console.log(obj);
+    /*  const json = queryString.parse('user=blue&pass=123&age=18');
+     console.log(json); */
     const fileName = './www' + req.url;
     fs.readFile(fileName, (err, data) => {
         if(err){
---
+++ server.post.js
@@ -0,0 +1,6 @@
+const http = require('http');
+http.createServer((req, res) => {
+    // post-req on接受post
+    req.on('data',()=>{});
+    req.end
+}).listen('8031');
